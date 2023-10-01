866f8119f6ada56671ef4dbe4d115692400dae9c
Ahmed Ehab <ahmedmedo012556@gmail.com>
2023-10-01 04:48:22 +0300

    success message added

--- js/destroyerror.js
+++ js/destroyerror.js
@@ -2,10 +2,10 @@ setTimeout(() => {
     const errorm = document.getElementById('errorm');
     errorm.style.display = 'none';
     window.open('destroyerror.php');
-}, 4000);
+}, 3000);
 
 setTimeout(() => {
     const errorm = document.getElementById('sucm');
     errorm.style.display = 'none';
     window.open('destroyerror.php');
-}, 4000);
+}, 3000);
