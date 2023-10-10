7038e2f98b9a86218ebe636340260f20d1274c64
Ahmed Ehab <ahmedmedo012556@gmail.com>
2023-10-11 02:18:37 +0300

    deleted a not needed code for now

--- html/admin/profile.php
+++ html/admin/profile.php
@@ -212,33 +212,6 @@
                             {
                                 echo time_ago($data['updated_at']);
                             }
-
-
-                            // echo "<div id = 'date'>";
-                            //     if(date("Y-m-d H:i:s") > "2023-10-09 17:40:00")
-                            //     {
-                            //         echo 
-                            //         '<div id="overlay">
-                            //             <div id="content">
-                            //                 <p class = "text">Register Closed Now</p>
-                            //             </div>
-                            //         </div>';
-                            //     }
-                            //     else
-                            //     {
-                            //             //Set the date and time
-                            //             $date = new DateTime('2023-10-09 17:40:00');
-
-                            //             //Get the current date and time
-                            //             $now = new DateTime();
-
-                            //              Calculate the difference between the current date and time and the target date and time
-                            //             $interval = $date->diff($now);
-
-                            //             //Display the remaining time
-                            //             echo 'Remaining time: ' . $interval->format('%h hours, %i minutes, %s seconds');
-                            //     }
-                            // echo "</div>";
                         ?>
                     </b></p>
             </div>
@@ -263,11 +236,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <script src = "../../js/destroyerror.js"></script>
-    <script>
-        setInterval(function() {
-            $('#date').load('profile.php #date');
-        }, 1000);
-    </script>
 </body>
 
 </html>
